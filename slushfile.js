e8c1edc764cd261b5631ae30f1bbe11b7bfa00f0
rehorn <sysucgh@gmail.com>
2014-07-17 23:44:56 +0800

    fix = =||

--- slushfile.js
+++ slushfile.js
@@ -7,45 +7,44 @@ var gulp = require('gulp'),
     inquirer = require('inquirer');
 
 gulp.task('default', function(done) {
-        gutil.log('slush-alloyteam-simple');
+    gutil.log('slush-alloyteam-simple');
 
-        var tplFiles = function(file) {
-            var basename = path.basename(file.path);
-            return basename === 'project.js';
-        };
+    var tplFiles = function(file) {
+        var basename = path.basename(file.path);
+        return basename === 'project.js';
+    };
 
-        inquirer.prompt([{
-                type: 'input',
-                name: 'name',
-                message: 'WebApp名称 -> '
-            }, {
-                type: 'input',
-                name: 'webServer',
-                message: 'html根目录，如: http://find.qq.com/qqun/search/ -> '
-            }, {
-                type: 'input',
-                name: 'cdn',
-                message: 'cdn根目录，如: http://s.url.cn/qqun/qqfind/search/ -> '
-            }, {
-                type: 'input',
-                name: 'subMoudle',
-                message: '子模块名字或留空 -> '
-            }, {
-                type: 'confirm',
-                name: 'moveon',
-                message: '开始初始化项目? -> '
-            }],
-            function(answers) {
-                if (!answers.moveon) {
-                    return done();
-                }
-                gulp.src(__dirname + '/templates/**') // Note use of __dirname to be relative to generator
-                .pipe(gulpif(tplFiles, template(answers)) // Lodash template support
-                    .pipe(gulp.dest('./')) // Without __dirname here = relative to cwd
-                    // .pipe(install()) // Run `bower install` and/or `npm install` if necessary
-                    .on('finish', function() {
-                        done(); // Finished!
-                    });
-                });
+    inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'WebApp名称 -> '
+        }, {
+            type: 'input',
+            name: 'webServer',
+            message: 'html根目录，如: http://find.qq.com/qqun/search/ -> '
+        }, {
+            type: 'input',
+            name: 'cdn',
+            message: 'cdn根目录，如: http://s.url.cn/qqun/qqfind/search/ -> '
+        }, {
+            type: 'input',
+            name: 'subMoudle',
+            message: '子模块名字或留空 -> '
+        }, {
+            type: 'confirm',
+            name: 'moveon',
+            message: '开始初始化项目? -> '
+        }],
+        function(answers) {
+            if (!answers.moveon) {
+                return done();
+            }
+            gulp.src(__dirname + '/templates/**') // Note use of __dirname to be relative to generator
+            .pipe(gulpif(tplFiles, template(answers))) // Lodash template support
+            .pipe(gulp.dest('./')) // Without __dirname here = relative to cwd
+            // .pipe(install()) // Run `bower install` and/or `npm install` if necessary
+            .on('finish', function() {
+                done(); // Finished!
+            });
         });
 });
